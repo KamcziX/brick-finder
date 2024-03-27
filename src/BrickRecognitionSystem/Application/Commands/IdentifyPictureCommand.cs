6bca4d94db6f288e70550df576162f878dafbf78
Kamil Krzyszowski <kamil.krzyszowski@inetum.com>
2024-03-27 14:32:45 +0100

    feature: Added IdentifyPictureCommand.cs

---
+++ src/BrickRecognitionSystem/Application/Commands/IdentifyPictureCommand.cs
@@ -0,0 +1,21 @@
+﻿using BrickManager.BrickRecognitionSystem.Application.Dto;
+using MediatR;
+
+namespace BrickManager.BrickRecognitionSystem.Application.Commands;
+
+/// <summary>
+/// Command used in picture identification process
+/// </summary>
+/// <param name="Picture"></param>
+public record IdentifyPictureCommand(string Picture) : IRequest<IdentificationResultDto>;
+
+/// <inheritdoc cref="IdentifyPictureCommand"/>
+public class IdentifyPictureCommandHandler() 
+    : IRequestHandler<IdentifyPictureCommand, IdentificationResultDto>
+{
+    public async Task<IdentificationResultDto> Handle(IdentifyPictureCommand request, 
+        CancellationToken cancellationToken)
+    {
+        return default;
+    }
+}
---
+++ src/BrickRecognitionSystem/Application/Dto/IdentificationResultDto.cs
@@ -0,0 +1,7 @@
+﻿namespace BrickManager.BrickRecognitionSystem.Application.Dto;
+
+/// <summary>
+/// Data transfer object used to store a final result of identification lego bricks from picture process. 
+/// </summary>
+/// <param name="IdentifiedBricks">A <see cref="Dictionary{TKey,TValue}"/> of all identified bricks and their respective quantities</param>
+public record IdentificationResultDto(IDictionary<string, int> IdentifiedBricks);
