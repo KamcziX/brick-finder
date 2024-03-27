b1ebc141b2dafc40c47170ec1dd013beb1dfd13c
Kamil Krzyszowski <kamil.krzyszowski@inetum.com>
2024-03-27 13:40:58 +0100

    feature: Added BrickRecognitionSystem

---
+++ src/BrickRecognitionSystem/Api/Program.cs
@@ -0,0 +1,25 @@
+﻿using BrickManager.BrickRecognitionSystem.Api;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Starting Brick Recognition System");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+var configurationBuilder = new ConfigurationBuilder()
+    .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+    .AddJsonFile($"appSettings.{builder.Environment.EnvironmentName}.json", false, true)
+    .Build();
+    
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+builder.Host.UseContentRoot(Directory.GetCurrentDirectory());
+
+var app = builder.Build();
+
+startup.Configure(app, app.Environment);
+app.Run();
---
+++ src/BrickRecognitionSystem/Api/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BrickManager.BrickRecognitionSystem.Api;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddHttpClient();
+        serviceCollection.AddOptions();
+        
+        serviceCollection.AddHealthChecks();
+        serviceCollection.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder applicationBuilder,
+        IWebHostEnvironment webHostEnvironment)
+    {
+        applicationBuilder.UseHsts();
+        applicationBuilder.UseRouting();
+        
+        applicationBuilder.UseEndpoints(
+            endpointRouteBuilder => {                 
+                endpointRouteBuilder.MapHealthChecks("/healthcheck")
+                    .WithMetadata(new AllowAnonymousAttribute());});
+    }
+}
